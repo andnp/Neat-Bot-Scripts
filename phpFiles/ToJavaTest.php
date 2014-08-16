32a70d46a47d20e90897fd5bf8900a0e90be5d32
andnp <soundguyandyp@gmail.com>
2014-08-15 22:52:16 -0400

    Made modular

--- phpFiles/ToJavaTest.php
+++ phpFiles/ToJavaTest.php
@@ -1,18 +1,24 @@
 <?PHP
-$host = "tcp://localhost"; 
-$port = 6789;
-$data = 'test' . PHP_EOL;
-$errstr = '';
-$errno = '';
+echo sendDataToJava("text");
 
-if ( ($fp = fsockopen($host, $port, $errno, $errstr, 3) ) === FALSE)
-    echo "$errstr ($errno)";
-else {
-    print 'SUCCESS!<br />';
-    fwrite($fp, $data);
-    while (! feof($fp)) {
-      echo fgets($fp, 4096);
-    }
-    fclose($fp);
+function sendDataToJava($data){
+	$host = "tcp://localhost"; 
+	$port = 6789;
+	$data = $data . PHP_EOL;
+	$errstr = '';
+	$errno = '';
+	$return = "";
+
+	if ( ($fp = fsockopen($host, $port, $errno, $errstr, 3) ) === FALSE)
+		echo "$errstr ($errno)";
+	else {
+		//print 'SUCCESS!<br />';
+		fwrite($fp, $data);
+		while (! feof($fp)) {
+		  $return = fgets($fp, 4096);
+		}
+		fclose($fp);
+	}
+	return $return;
 }
 ?>
